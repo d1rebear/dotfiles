653a89d62bb0ea0eda9b62fbbf9f7eed30687c47
d1rebear <19630734+d1rebear@users.noreply.github.com>
2023-07-12 12:49:33 +0300

    nvim: add leap plugin

--- nvim/lua/d1rebear/packer.lua
+++ nvim/lua/d1rebear/packer.lua
@@ -37,5 +37,6 @@ return require('packer').startup(function(use)
             { 'L3MON4D3/LuaSnip' },
         }
     }
+    use('ggandor/leap.nvim')
     --TODO: lazy load as many plugins as possible
 end)
